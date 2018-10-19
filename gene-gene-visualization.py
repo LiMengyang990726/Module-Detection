018cd469b2462f942a0847ec676ba695ec70de0b
Li Mengyang <LIME0040@e.ntu.edu.sg>
2018-10-19 11:49:01 +0800

    Refined visualization python file

--- gene-gene-visualization.py
+++ gene-gene-visualization.py
@@ -1,6 +1,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.colors as col
+import warnings
+# for filer the numpy binary imcompatibilty warning in linux server
+warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 G = nx.read_edgelist("./gene-network.tsv",comments = "#", nodetype = int)
 pos = nx.spring_layout(G)
@@ -23,64 +27,22 @@ for line in disease_file:
 # if they are in the same community, same color
 # if not covered by any community, grey
 i = 10 # random starting point for the color changing
-for key in disease_dic.keys():
+for key in disease_dic.keys(): # draw the hightlighted nodes
     nx.draw_networkx_nodes(G,pos,
                            nodelist=disease_dic[key],
                            node_size=500,
-                           node_color=col.Colormap(plasma,N=i)
-                           alpha=0.8,
+                           node_color=col.Colormap(plasma,N=i),
+                           edge_color=col.Colormap(plasma,N=i),
                            # cmap=plt.cm.plasma)
                            )
     i++
-
-# find those nodes that should be added to each network according to their connectivity significance in the first round
+# draw those that aren't mentioned
+all_nodes = G.nodes()
 for key in disease_dic.keys():
-    thisList = disease_dic[key]
-
-
-# gamma(z) = (z-1)!
-def compute_all_gamma_ln(N):
-
-    gamma_ln = {}
-    for i in range(1,N+1):
-        gamma_ln[i] = scipy.special.gammaln(i)
-
-    return gamma_ln
-
-# =============================================================================
-def logchoose(n, k, gamma_ln):
-    if n-k+1 <= 0:
-        return scipy.infty
-    lgn1  = gamma_ln[n+1]
-    lgk1  = gamma_ln[k+1]
-    lgnk1 = gamma_ln[n-k+1]
-    return lgn1 - [lgnk1 + lgk1]
-
-# =============================================================================
-def gauss_hypergeom(x, r, b, n, gamma_ln):
-    return np.exp(logchoose(r, x, gamma_ln) +
-                  logchoose(b, n-x, gamma_ln) -
-                  logchoose(r+b, n, gamma_ln))
-
-# =============================================================================
-def pvalue(kb, k, N, s, gamma_ln):
-    """
-    -------------------------------------------------------------------
-    Computes the p-value for a node that has kb out of k links to
-    seeds, given that there's a total of s sees in a network of N nodes.
-
-    p-val = \sum_{n=kb}^{k} HypergemetricPDF(n,k,N,s)
-    -------------------------------------------------------------------
-    """
-    p = 0.0
-    for n in range(kb,k+1):
-        if n > s:
-            break
-        prob = gauss_hypergeom(n, s, N-s, k, gamma_ln)
-        # print prob
-        p += prob
-
-    if p > 1:
-        return 1
-    else:
-        return p
+    for i in disease_dic[key]:
+        all_nodes.remove(disease_dic[key][i])
+nx.draw_networkx_nodes(G,pos,
+                       nodelist= all_nodes,
+                       node_size=500,
+                       node_color='gray',
+                       alpha=0.8)
---
+++ terminal_operations.md
@@ -0,0 +1,29 @@
+`pwd` : query for current working directory
+
+`mkdir`: make a directory (in the current working directory)
+
+`touch`: create an empty file
+
+`open`: open the file with the default editor
+
+`cp <original-file> <copy-file>`: to copy the file
+
+`cp <dir> <copy-dir>`: to copy a directory (copy-dir has to exist first)
+
+`mv <old-name> <new-name>`: to rename a file
+
+`mv <file> <path>`: to move a file
+
+`mv <file> <path/new-name>`: to move it and rename it. E.g. `mv orgin.txt ../text.txt`
+
+`rm`: to remove a file (different from removing from the finder)
+
+`man <command-name>`: to see the options under this command
+
+`rm`: remove file
+
+`rmdir`: remove directory
+
+`rm -r`: recursively remove
+
+`rm -rf`: recursively remove and forced remove (for both file and directory)
