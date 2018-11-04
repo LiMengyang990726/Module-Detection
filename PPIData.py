843d62a4e682e81edcf728befe669d006329ad5c
Li Mengyang <LIME0040@e.ntu.edu.sg>
2018-11-04 23:33:26 +0800

    Seeking for proper way to convert data into feature

--- PPIData.py
+++ PPIData.py
@@ -6,12 +6,29 @@ import collections
 import subprocess as sp # for clear console: tmp = sp.call('clear',shell=True)
 
 # ===============================================================
-# Network Analysis
+# Input
 # ===============================================================
 G = nx.read_edgelist("./gene-network.tsv")
-f= open("Network_Analysis.txt","w+")
-f.write(nx.info(G))
+Gc = max(nx.connected_component_subgraphs(G), key=len)
 
+disease_dic = {}
+disease_file  = open("gene-disease0.TSV", 'r')
+for line in disease_file:
+    li = line.strip()
+    if not li.startswith("#"):
+        li2 = li.split(' ',1)
+        disease_key = li2[0]
+        print ("the key is: "+disease_key)
+        disease_list = [l for l in (li2[1]).split('/')]
+        print (disease_list)
+        disease_dic.update({disease_key: disease_list})
+
+# ===============================================================
+# Network Analysis
+# ===============================================================
+f= open("Network_Analysis.txt","w+")
+
+f.write(nx.info(G))
 f.write("\nAverage clustering coefficient " + repr(nx.average_clustering(G)))
 
 plt.hist(list(dict(nx.degree(G)).values()))
@@ -21,14 +38,13 @@ plt.savefig("Degree_distribution.png")
 degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
 f.write("\nLargest degree " + str(degree_sequence[0]))
 
-Gc = max(nx.connected_component_subgraphs(G), key=len)
 f.write("\nThe largest connected component ")
 f.write("\n\t\tNumber of nodes " + str(len(Gc)))
 f.write("\n\t\tNumber of edges " + str(Gc.number_of_edges()))
 f.write("\n\t\tAverage shortest path length " + str(nx.average_shortest_path_length(Gc)))
 f1 = open("Edge_Betweenness.txt","w+")
-f1.write(nx.edge_betweenness_centrality(Gc))
-for line in f1:
+# f1.write(nx.edge_betweenness_centrality(Gc))
+# for line in f1:
 
 f.close()
 
@@ -69,3 +85,12 @@ f1.close()
 # ===============================================================
 # Convert Data Into Features
 # ===============================================================
+for key in disease_dic:
+    l = len(disease_dic[key])
+    nodes = []
+    for i in range(l):
+        if disease_dic[key][i] in Gc.nodes:
+            nodes.append(int(disease_dic[key][i]))
+    sub = Gc.subgraph(nodes)
+    n = nx.number_connected_components(sub)
+    print("number of connected components is " + str(n) + "\n")
