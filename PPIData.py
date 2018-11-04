6704b10b164b9baef4d1db18f236982a4ea10a17
Li Mengyang <LIME0040@e.ntu.edu.sg>
2018-11-04 19:38:04 +0800

    Percentage of disease gene in LCC

--- PPIData.py
+++ PPIData.py
@@ -30,28 +30,42 @@ f1 = open("Edge_Betweenness.txt","w+")
 f1.write(nx.edge_betweenness_centrality(Gc))
 for line in f1:
 
-
 f.close()
 
 # ===============================================================
-# Network Analysis
+# Percentage Analysis
 # ===============================================================
-disease_dic = {}
-disease_file  = open("gene-disease0.TSV", 'r')
-for line in disease_file:
-    li = line.strip()
-    if not li.startswith("#"):
-        li2 = li.split(' ',1)
-        disease_key = li2[0]
-        print ("the key is: "+disease_key)
-        disease_list = [l for l in (li2[1]).split('/')]
-        print (disease_list)
-        disease_dic.update({disease_key: disease_list})
+import xlsxwriter
 
-D = []
+f1 = xlsxwriter.Workbook('Percentage_of_Disease_Gene_in_PPI.xlsx')
+sh = f1.add_worksheet()
+
+sh.write(0,0,'Disease Name')
+sh.write(0,1,'Number of disease genes')
+sh.write(0,2,'Number of disease genes inside LCC')
+sh.write(0,3,'Percentage of number of disease genes')
+
+row = 1
+col = 0
+count = 0
+cell_format1 = f1.add_format()
+cell_format2 = f1.add_format()
+cell_format1.set_num_format('0')
+cell_format2.set_num_format('0.00000000000')
 for i in disease_dic:
+    sh.write_string(row,col,i)
+    temp = 0
+    length = len(disease_dic[i])
     for j in disease_dic[i]:
-        temp = 0
         if j in list(Gc):
-            temp += 1
-        D.append(temp)
+            temp+=1
+    sh.write_number(row,col+1,length,cell_format1)
+    sh.write_number(row,col+2,temp,cell_format1)
+    sh.write_number(row,col+3,(temp*1.0)/length,cell_format2)
+    row += 1
+
+f1.close()
+
+# ===============================================================
+# Convert Data Into Features
+# ===============================================================
