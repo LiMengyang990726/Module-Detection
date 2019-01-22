237fa8c62e980317933fb10fdb340ca8e68bf592
LiMengyang990726 <lime0040@e.ntu.edu.sg>
2019-01-22 14:11:28 +0800

    DataPreprocessing

--- NewTopologicalFeatures/ML.py
+++ NewTopologicalFeatures/ML.py
@@ -1,18 +1,29 @@
-import os
-import warnings
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-from http import HTTPStatus
-
-# from pypi
-import matplotlib
 import pandas as pd
-import requests
-import seaborn
 import csv
-import matplotlib.pyplot as plt
-from tabulate import tabulate
 
-topoData = pd.read_csv("allTopoFeatures.csv")
+# Step 1: Do feature scaling
+# Result has already been saved to topoFeatures_norm
+topoFeatures_norm = pd.read_csv('topoFeatures_norm')
 
-# Do feature scaling
-# To make the dataset looks a bit more Gaussian (for the purpose of doing anomaly detection), can do twisting (like take log)
+# Step 2: Principle Component Analysis
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(topoFeatures_norm)
+principalDf = pd.DataFrame(data = principalComponents
+             , columns = ['principal component 1', 'principal component 2'])
+                # visualize
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['x','y']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
