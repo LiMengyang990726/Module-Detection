aba641770b26937a090ca17d5f363327bb1ac241
Li Mengyang <LIME0040@e.ntu.edu.sg>
2019-06-13 23:20:28 +0800

    Added New Dataset Input Files

--- re_feeding_feature_generation.py
+++ re_feeding_feature_generation.py
@@ -22,36 +22,59 @@ def selectedTopoFeatures(G, path,disease_genes):
 
 def selectedSequenceFeatures(inputPath,outputPath):
     seq_file_dis = os.path.join(inputPath, "disgenes_sequence.txt")     # These two files haven't been obtained. Need to get the sequence coversion form Uniprot
-    seq_file_non_dis = os.path.join(inputPath, "endgenes_sequence.txt") # These two files haven't been obtained. Need to get the sequence coversion form Uniprot
+    seq_file_non_dis = os.path.join(inputPath, "nondisease_genes_sequence.txt") # These two files haven't been obtained. Need to get the sequence coversion form Uniprot
     seq_output_dis = os.path.join(outputPath,"Disease_genes_sequence_features.csv")
     seq_output_non_dis = os.path.join(outputPath, "Non_disease_genes_sequence_features.csv")
     #
     computeSequenceFeatures(seq_file_dis, seq_output_dis)
     computeSequenceFeatures(seq_file_non_dis, seq_output_non_dis)
 
-def selectedFunctionalFeatures():
-    # Require Ms Rama's file
+def selectedFunctionalFeatures(inputPath,outputPath):
+    dis_bp = pd.read_table(os.path.join(inputPath, 'asd_bp'), delim_whitespace=True, names=('ProteinID', 'BP'))
+    dis_cc = pd.read_table(os.path.join(inputPath, 'asd_cc'), delim_whitespace=True, names=('ProteinID', 'CC'))
+    dis_mf = pd.read_table(os.path.join(inputPath, 'asd_mf'), delim_whitespace=True, names=('ProteinID', 'MF'))
 
-def dataCombination():
-    eigen = pd.read_csv("/Users/limengyang/Workspaces/Re-feeding Using New Dataset/EigenvectorCentrality.csv")
-    modu = pd.read_csv("/Users/limengyang/Workspaces/Re-feeding Using New Dataset/Modularity.csv")
-    page = pd.read_csv("/Users/limengyang/Workspaces/Re-feeding Using New Dataset/PageRank.csv")
+    non_dis_bp = pd.read_table(os.path.join(inputPath, 'nm_bp'), delim_whitespace=True, names=('ProteinID', 'BP'))
+    non_dis_cc = pd.read_table(os.path.join(inputPath, 'nm_cc'), delim_whitespace=True, names=('ProteinID', 'CC'))
+    non_dis_mf = pd.read_table(os.path.join(inputPath, 'nm_mf'), delim_whitespace=True, names=('ProteinID', 'MF'))
 
-    seq_dis = pd.read_csv("/Users/limengyang/Workspaces/Re-feeding Using New Dataset/Disease_genes_sequence_features.csv")
-    seq_non_dis = pd.read_csv("/Users/limengyang/Workspaces/Re-feeding Using New Dataset/Non_disease_genes_sequence_features.csv")
+    dis_bpcc = pd.merge(dis_bp, dis_cc, on='ProteinID', how='outer')
+    dis = pd.merge(dis_bpcc, dis_mf, on='ProteinID', how='outer')
+    dis.head()
 
-    function = pd.read_csv("")
+    non_dis_bpcc = pd.merge(non_dis_bp, non_dis_cc, on='ProteinID', how='outer')
+    non_dis = pd.merge(non_dis_bpcc, non_dis_mf, on='ProteinID', how='outer')
+    non_dis.head()
+
+    frames = [dis, non_dis]
+    allFunctioinalFeatures = pd.concat(frames)
+    allFunctioinalFeatures.to_csv(os.path.join(outputPath, "allFunctionalFeatures.csv"))
+
+def dataCombination(outputPath):
+    eigen = pd.read_csv(os.path.join(outputPath, "EigenvectorCentrality.csv"))
+    modu = pd.read_csv(os.path.join(outputPath,"Modularity.csv"))
+    page = pd.read_csv(os.path.join(outputPath,"PageRank.csv"))
+
+    seq_dis = pd.read_csv(os.path.join(outputPath,"Disease_genes_sequence_features.csv"))
+    seq_non_dis = pd.read_csv(os.path.join(outputPath,"Non_disease_genes_sequence_features.csv"))
+
+    function = pd.read_csv(os.path.join(outputPath,"allFunctionalFeatures.csv"))
 
     frames = [eigen, modu, page, seq_dis, seq_non_dis, function]
+    result = pd.concat(frames)
+    result.to_csv(os.path.join(outputPath, "AllFeatures.csv"))
+    return result
 
-    return frames
+def dataCleaning(df):
+    # Do the cleaning process
+    return df
 
-def dataCleaning():
+def feedRandomForestModel():
 
 def computeSequenceFeatures(input_file, output_file):
     # read ids and sequences in a dictionary
     seq_data=np.genfromtxt(input_file, dtype=str, delimiter='\t')
-
+    #
     # write output
     f = open(output_file, mode='w')
     fieldnames = ['Protein_ID', 'FrequencyA','FrequencyB',
@@ -65,17 +88,17 @@ def computeSequenceFeatures(input_file, output_file):
                     'FrequencyX','FrequencyY','FrequencyZ',
                     'Aromaticity','SSfraction']
     f_writer = csv.DictWriter(f, fieldnames=fieldnames)
-
+    #
     for p in range(len(seq_data)):
         protein_id = seq_data[p][0]
         seq = seq_data[p][1]
-
+        #
         frequency = SequenceFeatures.getAAfreq(seq)
         frequencies = [0] * 26
         for key,value in frequency.items():
             alpha = ord(key) - 65
             frequencies[alpha] = value
-
+        #
         aromaticity = SequenceFeatures.getAromaticity(seq)
         ssfraction = SequenceFeatures.getSSfraction(seq)
         f_writer.writerow({'Protein_ID': protein_id,
@@ -113,12 +136,18 @@ def main():
     # Get Input
     G, disease_genes, non_disease_genes = readInput3("./data/ASD_data/")
 
+    inputPath = "./data/ASD_data/"
+    outputPath = "./Re-feeding Using New Dataset/"
+
     # Topological Features
-    selectedTopoFeatures(G, "./Re-feeding Using New Dataset/",disease_genes)
+    selectedTopoFeatures(G, outputPath,disease_genes)
 
     # Sequential Features
-    inputPath = "/Users/limengyang/Workspaces/Module-Detection/data/ASD_data/"
-    outputPath = "/Users/limengyang/Workspaces/Re-feeding Using New Dataset/"
     selectedSequenceFeatures(inputPath,outputPath)
 
     # Functional Features
+    selectedFunctionalFeatures(inputPath, outputPath)
+
+    # Concat all Features
+    df = dataCombination(outputPath)
+    df = dataCleaning(df)
